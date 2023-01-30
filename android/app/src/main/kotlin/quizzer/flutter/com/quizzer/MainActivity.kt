9c1aea15c55e43fc9d0dd49c842b619120d64c4d
kumarc21 <Chandankumar489@gmail.com>
2023-01-30 16:39:00 +0530

    quizzer

---
+++ android/app/src/main/kotlin/quizzer/flutter/com/quizzer/MainActivity.kt
@@ -0,0 +1,6 @@
+package quizzer.flutter.com.quizzer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const QuizzerApp());
+
+class QuizzerApp extends StatelessWidget {
+  const QuizzerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Quiz'),
+          backgroundColor: Colors.teal,
+        ),
+        backgroundColor: Colors.grey,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25.0, color: Colors.black),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green, // background
+                foregroundColor: Colors.black, // foreground
+              ),
+              child: const Text('True'),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange, // background
+                foregroundColor: Colors.black, // foreground
+              ),
+              child: const Text('False'),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
